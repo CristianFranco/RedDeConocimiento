05706a1b15fef201b5f52a56145561269209fef1
kevin <kevin_rfx8@hotmail.com>
2016-05-10 23:00:06 -0500

    no message

--- Source/registro.php
+++ Source/registro.php
@@ -3,10 +3,11 @@
     require("procesos/connection.php");
     
 ?>
-<!DOCTYPE html>
+    <!DOCTYPE html>
     <html lang="es">
-<head>
-     <meta charset="utf-8">
+
+    <head>
+        <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,61 +23,62 @@
         <!--Import jQuery before materialize.js-->
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
-       
-</head>
-<body>
-    
-    <h2>Registro de Datos</h2>
-<div class="row">
- <form class="col s12" method="post" name="registro" action="envreg.php">
-     <div class="row">
-        <div class="input-field col s6">
-          <i class="material-icons prefix">account_circle</i>
-          <input id="icon_prefix" type="text" class="validate" name="nombre">
-          <label for="icon_prefix">Nombre</label>
-        </div>
-        <div class="input-field col s6">
-            <i class="material-icons prefix">account_circle</i>
-            <input id="icon_prefix" type="text" class="validate" name="apell">
-            <label for="icon_prefix">Apellido</label>
-        </div>
-         </div>
-     <div class="row">
-        <div class="input-field col s7">
-          <i class="material-icons prefix">email</i>
-          <input id="icon_prefix" type="text" class="validate" name="email">
-          <label for="icon_prefix">Email</label>
-        </div>
-        <div class="input-field col s5">
-          <i class="material-icons prefix">phone</i>
-          <input id="icon_prefix" type="text" class="validate" name="tel">
-          <label for="icon_prefix">Telefono</label>
-        </div>
-     </div>
-     <div class="row">
-        <div class="input-field col s7">
-          <i class="material-icons prefix">assignment_ind</i>
-          <input id="icon_prefix" type="text" class="validate" name="nkname">
-          <label for="icon_prefix">NickName</label>
-        </div>
-     </div>
-     <div class="row">
-        <div class="input-field col s7">
-          <i class="material-icons prefix">vpn_key</i>
-          <input id="icon_prefix" type="password" class="validate" name="pass">
-          <label for="icon_prefix">Contraseña</label>
-        </div>
-     </div>
-     <div class="row">
-        <div class="input-field col s7">
-          <i class="material-icons prefix">vpn_key</i>
-          <input id="icon_prefix" type="password" class="validate" name="confpass">
-          <label for="icon_prefix">Confirmar Contraseña</label>
-        </div>
-         
-     </div>
-         <div class="input-field col s5">
-        <?php
+
+    </head>
+
+    <body>
+
+        <h2>Registro de Datos</h2>
+        <div class="row">
+            <form class="col s12" method="post" name="registro" action="envreg.php">
+                <div class="row">
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="icon_prefix" type="text" class="validate" name="nombre">
+                        <label for="icon_prefix">Nombre</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="icon_prefix" type="text" class="validate" name="apell">
+                        <label for="icon_prefix">Apellido</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s7">
+                        <i class="material-icons prefix">email</i>
+                        <input id="icon_prefix" type="text" class="validate" name="email">
+                        <label for="icon_prefix">Email</label>
+                    </div>
+                    <div class="input-field col s5">
+                        <i class="material-icons prefix">phone</i>
+                        <input id="icon_prefix" type="text" class="validate" name="tel">
+                        <label for="icon_prefix">Telefono</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s7">
+                        <i class="material-icons prefix">assignment_ind</i>
+                        <input id="icon_prefix" type="text" class="validate" name="nkname">
+                        <label for="icon_prefix">NickName</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s7">
+                        <i class="material-icons prefix">vpn_key</i>
+                        <input id="icon_prefix" type="password" class="validate" name="pass">
+                        <label for="icon_prefix">Contraseña</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s7">
+                        <i class="material-icons prefix">vpn_key</i>
+                        <input id="icon_prefix" type="password" class="validate" name="confpass">
+                        <label for="icon_prefix">Confirmar Contraseña</label>
+                    </div>
+
+                </div>
+                <div class="input-field col s5">
+                    <?php
             $query='Select * From Pais order by Nombre ASC;';
             $connection=connect();
             $result=$connection -> query($query);
@@ -86,21 +88,23 @@
             }   
             echo "</select>";
         ?>
-         <label>Pais:</label>
-     </div>
-     
-     
-        <!--
+                        <label>Pais:</label>
+                </div>
+                <div class="input-field col s5">
+                    <label>Ciudad:</label>
+                    <select name="ciudades" id="ciudades">
+                        <option>Seleccionar Ciudad...</option>
+                    </select>
+                </div>
+
+                <!--
      <div  class="input-field col s6">
      <select name="pais" id="paises">
             <option disabled selected>Seleccionar Pais...</option>   
       </select>
          <label>Pais:</label>
      </div>
-        <label>Ciudad:</label>
-        <select name="ciudades" id="ciudades">
-            <option>Seleccionar Ciudad...</option>
-        </select>
+        
          <br><br>
         <label>Contrase&ntilde;a:</label>
         <input type="password" name="pass" size="60" maxlength="45">
@@ -109,43 +113,40 @@
         <input type="password" name="confpass" size="60" maxlength="32">
       <br><br>
         <input type="submit"  name="enviar" value="Registrarse"-->
-</form>
-    
-</div>
-<script>
-    $("#paises").on("change",function(e){
-        console.log($("#paises").val());
-            $.ajax({
-                url: "procesos/getCiudades.php"
-                , method: "POST"
-                , data: {
-                    idPais: $("#paises").val()
-                }
-                , dataType: "JSON"
-                , success: function (result) {
-                    $('select').material_select()
-                    $("#ciudades").empty();
-                    for(var x=0;x<result.length;x++){
-                        $("#ciudades").append('<option value="'+result[x].Id+'">'+result[x].Nombre+'</select>');
+            </form>
+
+        </div>
+        <script>
+            $("#paises").on("change", function (e) {
+                console.log($("#paises").val());
+                $.ajax({
+                    url: "procesos/getCiudades.php"
+                    , method: "POST"
+                    , data: {
+                        idPais: $("#paises").val()
                     }
-                    console.log(result[0].Id+" "+result[0].Nombre);
-                }
+                    , dataType: "JSON"
+                    , success: function (result) {
+
+                        $("#ciudades").empty();
+                        for (var x = 0; x < result.length; x++) {
+                            $("#ciudades").append('<option value="' + result[x].Id + '">' + result[x].Nombre + '</select>');
+                        }
+                        $('select').material_select()
+                        console.log(result[0].Id + " " + result[0].Nombre);
+                    }
+                });
+
+            })
+        </script>
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js">
+        </script>
+        <script>
+            $(document).ready(function () {
+                $('select').material_select();
             });
-                    
-    })
-</script>
-     <!-- Compiled and minified JavaScript -->
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js">
-    </script>
-    <script>$('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-      alignment: 'left' // Displays dropdown with edge aligned to the left of button
-    }
-  );</script>
-</body>
-</html>
+        </script>
+    </body>
+
+    </html>
