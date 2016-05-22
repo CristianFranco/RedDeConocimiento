d31e113796ca6a8a9626e66e41aa7f54cd0ed4cf
kilerone <btr940103@hotmail.com>
2016-05-22 17:36:11 -0500

    editarPerfil

--- Source/editarPerfil.php
+++ Source/editarPerfil.php
@@ -5,7 +5,7 @@
     
     $Uid= 1;
     $connection = connect();
-    $sql = "SELECT * from Usuario WHERE idUsuario LIKE $Uid";
+    $sql = "SELECT  * from Usuario,Ciudad WHERE idUsuario = $Uid and Usuario.idCiudad=Ciudad.ID;";
     $result = $connection -> query($sql);
     $usuario = $result -> fetch_assoc();
 ?>
@@ -48,39 +48,39 @@
         <div class="row">
             <div class="input-field col s4">
               <i class="material-icons prefix">account_circle</i>
-              <input id="icon_prefix" type="text" class="validate" pattern="[A-Za-z0-9 ]{6,15}" title="Mínimo 6 caracteres, máximo 15 caracteres" name="nickname" required value="<?php echo $usuario ['Nickname'];?>">
-              <label for="icon_prefix">Nickname</label>
+              <input id="nickname" type="text" class="validate" pattern="[A-Za-z0-9 ]{6,15}" title="Mínimo 6 caracteres, máximo 15 caracteres" name="nickname" required value="<?php echo $usuario ['Nickname'];?>" disabled>
+              <label for="nickname">Nickname</label>
             </div>
             <div class="input-field col s4">
               <i class="material-icons prefix">vpn_key</i>
-              <input id="icon_prefix" type="password" class="validate" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Al menos 8 caracteres con un número, una mayúscula y minúscula" name="password" required value="<?php echo $usuario ['Password'];?>">
-              <label for="icon_prefix">Password</label>
+              <input id="password" type="password" class="validate" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Al menos 8 caracteres con un número, una mayúscula y minúscula" name="password" required value="<?php echo $usuario ['Password'];?>" disabled>
+              <label for="password">Password</label>
             </div>
             <div class="input-field col s4">
               <i class="material-icons prefix">vpn_key</i>
-              <input id="icon_prefix" type="password" class="validate"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Al menos 8 caracteres con un número, una mayúscula y minúscula" name="confirmar" required>
-              <label for="icon_prefix">Confirmar Password</label>
+              <input id="conf_pass" type="password" class="validate"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Al menos 8 caracteres con un número, una mayúscula y minúscula" name="conf_pass" required disabled>
+              <label for="conf_pass">Confirmar Password</label>
             </div>
             <div class="input-field col s4">
               <i class="material-icons prefix">perm_identity</i>
-              <input id="icon_prefix" type="text" class="validate" pattern="[A-Za-z ]{6,15}" title="Introduce tu nombre verdadero" name="nombre" required value="<?php echo $usuario ['Password'];?>">
-              <label for="icon_prefix">Nombre</label>
+              <input id="nombre" type="text" class="validate" pattern="[A-Za-z ]{6,15}" title="Introduce tu nombre verdadero" name="nombre" required value="<?php echo $usuario ['Nombre'];?>" disabled>
+              <label for="nombre">Nombre</label>
             </div>
             <div class="input-field col s4">
               <i class="material-icons prefix">perm_identity</i>
-              <input id="icon_prefix" type="text" class="validate" pattern="[A-Za-z ]{6,20}" title="Introduce tus apellidos" name="apellidos" required value="<?php echo $usuario ['Nombre'];?>">
-              <label for="icon_prefix">Apellidos</label>
+              <input id="apellidos" type="text" class="validate" pattern="[A-Za-z ]{6,20}" title="Introduce tus apellidos" name="apellidos" required value="<?php echo $usuario ['Apellidos'];?>" disabled>
+              <label for="apellidos">Apellidos</label>
             </div>
             <div class="input-field col s4">
               <i class="material-icons prefix">phone</i>
-              <input id="icon_prefix" type="text" class="validate" pattern="[0-9]{10,20}" title="Introduce solo datos númericos (lada + télefono)" name="telefono" required value="<?php echo $usuario ['Telefono'];?>">
-              <label for="icon_prefix">Télefono</label>
+              <input id="telefono" type="text" class="validate" pattern="[0-9]{10,20}" title="Introduce solo datos númericos (lada + télefono)" name="telefono" required value="<?php echo $usuario ['Telefono'];?>" disabled>
+              <label for="telefono">Télefono</label>
             </div>
         </div>
           <div class="input-field col s4">
               <i class="material-icons prefix">assignment</i>
-              <input id="icon_prefix" type="text" class="validate" pattern="[A-Za-z0-9 ]{10,30}" title="Introduce una breve descripción de ti " name="descripcion" required value="<?php echo $usuario ['Descripcion'];?>">
-              <label for="icon_prefix">Descripción</label>
+              <input id="descripcion" type="text" class="validate" pattern="[A-Za-z0-9 ]{10,30}" title="Introduce una breve descripción de ti " name="descripcion" required value="<?php echo $usuario ['Descripcion'];?>" disabled>
+              <label for="descripcion">Descripción</label>
             </div>
 <!--BD CATALOGO DE PAIS-CIUDAD-->
           <div class="input-field col s4">
@@ -89,24 +89,28 @@
               $query='Select * From Pais order by Nombre ASC;';
               $connection=connect();
               $result=$connection -> query($query);
-               echo "<select  name='pais' id='paises'>";   
+               echo "<select disabled  name='pais' id='paises'>";   
                while($fila=$result->fetch_array(MYSQLI_ASSOC))
                {
-                echo "<option value='".$fila['Codigo']."'>".$fila['Nombre']."</option>";
+                echo "<option value='".$fila['Codigo']."'";
+                if($usuario['CodigoPais']==$fila['Codigo'])
+                    echo "selected";
+                echo ">".$fila['Nombre']."</option>";
                }   
               echo "</select>";
             ?>
-<!--PAISES-->
+<!--Ciudades-->
            <label>Pais:</label>
            </div>
              <div class="input-field col s4">
               <i class="material-icons prefix">location_on</i>
                <?php
                 $afg="AFG";
-                $query="Select * From Ciudad where CodigoPais='AFG' order by Nombre ASC;";
+                $pais=$usuario['CodigoPais'];
+                $query="Select * From Ciudad where CodigoPais='$pais' order by Nombre ASC;";
                 $connection=connect();
                 $result=$connection -> query($query);
-                echo "<select  name='ciudades' id='ciudades'>";   
+                echo "<select  disabled name='ciudades' id='ciudades'>";   
                 while($fila=$result->fetch_array(MYSQLI_ASSOC)){
                     if($usuario['idCiudad']==$fila['ID']){
                 echo"<option value='".$fila['ID']."' selected>".$fila['Nombre']."</option>";
@@ -121,16 +125,9 @@
           <br><br>
           <p>  
 <!--BOTONES-->            
-              <button class="waves-effect waves-light btn" type="submit" name="action">Habilitar
-                <i class="material-icons right">button</i>       
-              </button>
-               <button class="btn waves-effect waves-light" type="submit" name="action">Aceptar
-                <i class="material-icons right">button</i>       
-              </button>
-               <button class="btn waves-effect waves-light" type="submit" name="action">Cancelar
-                <i class="material-icons right">button</i>       
-              </button>
-              <a class="waves-effect waves-light btn" type="submit" name="action">button</a>
+              <a class="waves-effect waves-light btn" id="habilitar" type="button" name="action">Habilitar</a>
+              <a class="waves-effect waves-light btn" id="aceptar" type="submit" name="action">Aceptar</a>
+              <a class="waves-effect waves-light btn" id="cancelar" type="button" name="action">Cancelar</a>
         </form>
      </div>
         <script>
@@ -155,6 +152,22 @@
                 });
 
             })
+//HABILITAR CAMPOS
+            $("#habilitar").on("click",function(e){
+                $("#nickname").removeAttr("disabled");
+                $("#password").removeAttr("disabled");
+                $("#conf_pass").removeAttr("disabled");
+                $("#nombre").removeAttr("disabled");
+                $("#apellidos").removeAttr("disabled");
+                $("#telefono").removeAttr("disabled");
+                $("#descripcion").removeAttr("disabled");
+                $("#paises").removeAttr("disabled");
+                $("#ciudades").removeAttr("disabled");
+            });
+//CANCELAR
+            $("#cancelar").on("click",function(e){
+               window.location="editarPerfil.php"; 
+            });
         </script>          
         <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js">
