c7e45fd70942daf82185282cc8fc3c8b28b2375e
kilerone <btr940103@hotmail.com>
2016-05-24 20:09:10 -0500

    editarPerfil

--- Source/editarPerfil.php
+++ Source/editarPerfil.php
@@ -55,44 +55,44 @@
     <div class="row">
       <form class="col s12" method="post" name="registro" action="envreg.php">
         <div class="row">
-            <div class="input-field col s4">
+            <div class="input-field col m4">
               <i class="material-icons prefix">account_circle</i>
               <input id="nickname" type="text" class="validate" pattern="[A-Za-z0-9 ]{6,15}" title="Mínimo 6 caracteres, máximo 15 caracteres" name="nickname" required value="<?php echo $usuario ['Nickname'];?>" disabled>
               <label for="nickname">Nickname</label>
             </div>
-            <div class="input-field col s4">
+            <div class="input-field col m4">
               <i class="material-icons prefix">vpn_key</i>
               <input id="password" type="password" class="validate" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Al menos 8 caracteres con un número, una mayúscula y minúscula" name="password" required value="<?php echo $usuario ['Password'];?>" disabled>
               <label for="password">Password</label>
             </div>
-            <div class="input-field col s4">
+            <div class="input-field col m4">
               <i class="material-icons prefix">vpn_key</i>
               <input id="conf_pass" type="password" class="validate"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Al menos 8 caracteres con un número, una mayúscula y minúscula" name="conf_pass" required disabled>
               <label for="conf_pass">Confirmar Password</label>
             </div>
-            <div class="input-field col s4">
+            <div class="input-field col m4">
               <i class="material-icons prefix">perm_identity</i>
               <input id="nombre" type="text" class="validate" pattern="[A-Za-z ]{6,15}" title="Introduce tu nombre verdadero" name="nombre" required value="<?php echo $usuario ['Nombre'];?>" disabled>
               <label for="nombre">Nombre</label>
             </div>
-            <div class="input-field col s4">
+            <div class="input-field col m4">
               <i class="material-icons prefix">perm_identity</i>
               <input id="apellidos" type="text" class="validate" pattern="[A-Za-z ]{6,20}" title="Introduce tus apellidos" name="apellidos" required value="<?php echo $usuario ['Apellidos'];?>" disabled>
               <label for="apellidos">Apellidos</label>
             </div>
-            <div class="input-field col s4">
+            <div class="input-field col m4">
               <i class="material-icons prefix">phone</i>
               <input id="telefono" type="text" class="validate" pattern="[0-9]{10,20}" title="Introduce solo datos númericos (lada + télefono)" name="telefono" required value="<?php echo $usuario ['Telefono'];?>" disabled>
               <label for="telefono">Télefono</label>
             </div>
         </div>
-          <div class="input-field col s4">
+          <div class="input-field col m4">
               <i class="material-icons prefix">assignment</i>
               <input id="descripcion" type="text" class="validate" pattern="[A-Za-z0-9 ]{10,30}" title="Introduce una breve descripción de ti " name="descripcion" required value="<?php echo $usuario ['Descripcion'];?>" disabled>
               <label for="descripcion">Descripción</label>
             </div>
 <!--BD CATALOGO DE PAIS-CIUDAD-->
-          <div class="input-field col s4">
+          <div class="input-field col m4">
             <i class="material-icons prefix">location_on</i>
              <?php
               $query='Select * From Pais order by Nombre ASC;';
@@ -111,7 +111,7 @@
 <!--Ciudades-->
            <label>Pais:</label>
            </div>
-             <div class="input-field col s4">
+             <div class="input-field col m4">
               <i class="material-icons prefix">location_on</i>
                <?php
                 $afg="AFG";
@@ -133,12 +133,20 @@
              </div>
           <br><br>
           <p>  
-<!--BOTONES-->    
-              <form class="col s12" method="post" name="registro" action="envreg.php">
-              <a class="waves-effect waves-light btn" id="habilitar" type="button" name="action">Habilitar</a>
-              <a class="waves-effect waves-light btn" id="aceptar" type="submit" name="action">Aceptar</a>
-              <a class="waves-effect waves-light btn" id="cancelar" type="button" name="action">Cancelar</a>
+<!--BOTONES-->   
+              <div class="row">
+              <form class="col m12" method="post" name="registro" action="envreg.php">
+              <div class="input-field col m2"> 
+                  <a class="waves-effect waves-light btn" id="habilitar" type="button" name="action">Habilitar</a>
+              </div>
+              <div class="input-field col m2">
+                  <a class="waves-effect waves-light btn" id="aceptar" type="submit" name="action">Aceptar</a>
+              </div>
+              <div class="input-field col m2">
+                  <a class="waves-effect waves-light btn" id="cancelar" type="button" name="action">Cancelar</a>
+              </div>
               </form>
+              </div>
         </form>
      </div>
             </div>
