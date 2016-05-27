0947c861bb35ae3c9ef1e85cef72094f5f468ac7
Marco Antonio León Juárez <paramore_malj@outlook.com>
2016-05-27 00:27:58 -0500

    página mostrar.php con procesos terminada

--- Source/procesos/borrarGrupo.php
+++ Source/procesos/borrarGrupo.php
@@ -1,10 +1,10 @@
 <?php
-require("procesos/connection.php");
+require("connection.php");
 $connection=connect();
 $idGrupo=$_POST['idGrupo'];
 $delete="DELETE FROM Grupo
 WHERE Grupo.idGrupo=$idGrupo;";
-$result4=$connection->query($sql);
-header("index.php");
+$result4=$connection->query($delete);
+header("../index.php");
 
 ?>
