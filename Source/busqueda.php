3315c07d8726dc64317278176a1c357c5626c9b9
CristianFranco <cfa_94@hotmail.com>
2016-05-30 07:15:24 -0500

    header

--- Source/busqueda.php
+++ Source/busqueda.php
@@ -1,10 +1,6 @@
-<?php 
-    session_start();
-?>
-    <!DOCTYPE html>
-    <html lang="es">
-
-    <head>
+<!DOCTYPE html>
+<html lang = "es">
+<head>
 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,100 +17,98 @@
 
         <!--Import jQuery before materialize.js-->
         <script src="https://code.jquery.com/jquery-2.1.0.min.js" integrity="sha256-8oQ1OnzE2X9v4gpRVRMb1DWHoPHJilbur1LP9ykQ9H0=" crossorigin="anonymous"></script>
-
+        
         <script>
             var idEstilo = <?php if(isset($_SESSION['idUsuario'])) echo $_SESSION['idUsuario']; else echo 0; ?>;
         </script>
         <script src="../JS/cargarPreferencias.js"></script>
         <link rel="stylesheet" type="text/css" href="../CSS/style.css">
     </head>
+<body >
+<?php 
+    session_start();
+?>
 
-    <body>
-
-
-        <header>
+ 		<header>
             <?php require("header.php") ?>
         </header>
         <main>
 
-            <div class="container secundario">
+	<div class="container secundario">
 
-                <div class="row">
-                    <div class="col s10"></div>
-                    <div class="col s1 offset-s11">
-                        <a href="#modal" class="material-icons modal-trigger">live_help</a>
-                    </div>
-                </div>
+	<div class="row">
+		<div class="col s10"></div>
+			<div class="col s1 offset-s11">
+	        			<a href="#modal" class="material-icons modal-trigger">live_help</a>
+	        </div>
+        </div>
 
-                <div class="modal" id="modal">
-                    <div class="container">
-                        <br>
-                        <h4> Esta información es con el fin para que cómpreda mejor el uso de la búsqueda avanzada.</h4>
-                        <br>
-                        <br>
-                        <ul style="list-style-type:square">
-                            <li>&#8226; En el campo de búsqueda o persona busca las personas que contengan las palabras especificadas.</li>
-                            <br>
-                            <br>
+        <div class="modal" id ="modal">
+        	<div class="container">
+        	<br>
+        	<h4> Esta información es con el fin para que cómpreda mejor el uso de la búsqueda avanzada.</h4>
+        	<br><br>
+<ul style="list-style-type:square">
+        	<li>&#8226; En el campo de búsqueda o persona busca las personas que contengan las palabras especificadas.</li>
+        	<br><br>
 
-                            <li>&#8226; La búsqueda por área de conocimiento es para traer los grupos los cuales se encuentran en esa área de conocimiento.</li>
+        	<li>&#8226; La búsqueda por área de conocimiento es para traer los grupos los cuales se encuentran en esa área de conocimiento.</li>
 
-                            <br>
-                            <br>
-                            <li>&#8226; La búsqueda por contenido de publicaciones trae los grupos que contienen publicaciones de ese contenido y los usuarios que han publicado ese tipo de publicaciones.</li>
-                            <br>
-                            <br>
-                            <li>&#8226; Al tener activada la opción de Grupos que contengan el tipo de publicación trae los grupos que se encuentren en un área de conocimiento y que contengan los contenidos de publicaciones que se especifica.</li>
-                            <br>
-                            <br>
-                            <li>&#8226; Al tener activada la opción Grupos o publicaciones trae los grupos que se encuentren en esa área de conocimiento y también los grupos que contengan publicaciones por búsqueda de contenido.</li>
-                            <br>
-                        </ul>
-                        <div class="row">
-                            <div class="col s2 offset-s10">
-                                <div class="action-bar">
-                                    <a href="#" class="btn-flat modal-action modal-close">Entendido</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        	<br><br>
+        	<li>&#8226; La búsqueda por contenido de publicaciones trae los grupos que contienen publicaciones de ese contenido y los usuarios que han publicado ese tipo de publicaciones.</li>
+<br><br>
+        	<li>&#8226; Al tener activada la opción de Grupos que contengan el tipo de publicación trae los grupos que se encuentren en un área de conocimiento y que contengan los contenidos de publicaciones que se especifica.</li>
+<br><br>
+        	<li>&#8226; Al tener activada la opción Grupos o publicaciones trae los grupos que se encuentren en esa área de conocimiento y también los grupos que contengan publicaciones por búsqueda de contenido.</li>
+<br>
+</ul>
+<div class="row">
+<div class= "col s2 offset-s10">
+        	<div class="action-bar">
+        		<a href="#" class="btn-flat modal-action modal-close">Entendido</a>
+        	</div>
+        </div>
+        </div>
+        	</div>
+        </div>
 
-                <form action="./resultadosDeBusqueda.php" method="POST" id='subFor'>
-                    <div class="input-field">
-                        <label class="icoP" for="nombre">Busqueda por nombre de grupo o persona:</label>
-                        <input class="inpP" type="text" id="nombre" name="nombre">
-                    </div>
+		<form action="./resultadosDeBusqueda.php" method="POST" id='subFor'>
+			<div class="input-field" >
+				<label class="icoP" for="nombre">Búsqueda por nombre de grupo o persona:</label>
+				<input class="inpP" type="text" id="nombre" name="nombre" ></input>
+			</div>
 
-                    <div class="input-field">
-                        <label class="icoP" for="area">Busqueda por area de conocimiento:</label>
-                        <input class="inpP" type="text" id="area" name="area">
-                    </div>
-                    <p>
-                        <input class="icoP" name="group1" type="radio" value="1" id="and" checked />
-                        <label class="icoP" for="and">Grupos que contengan el tipo de publicación</label>
-                    </p>
-                    <p>
-                        <input class="principal" name="group1" type="radio" id="or" value="2" />
-                        <label class="icoP" for="or">Grupos o publicaciones</label>
-                    </p>
-                    <div class="input-field">
-                        <label class="icoP" for="tags">Busqueda por contenido de publicaciones:</label>
-                        <input class="inpP" type="text" id="tags" name="tags">
-                    </div>
+			<div class="input-field" >
+			<label class="icoP" for="area">Búsqueda por área  de conocimiento:</label>
+				<input class="inpP" type="text" id="area" name="area"></input>
+			</div>
+			<p>
+			      <input class="icoP" name="group1" type="radio" value = "1" id="and" checked />
+			      <label class="icoP" for="and">Grupos que contengan el tipo de publicación</label>
+			    </p>
+			    <p>
+			      <input class="principal" name="group1" type="radio" id="or" value = "2" />
+			      <label class="icoP" for="or">Grupos o publicaciones</label>
+			    </p>
+			<div class="input-field" >
+			<label class="icoP" for="tags">Búsqueda  por contenido de publicaciones:</label>
+				<input class="inpP" type="text" id="tags"  name="tags"></input>
+			</div>
 
-                    <button class="btn waves-effect waves-light col s6 principal" type="submit" name="action">Buscar
-                        <i class="material-icons right">search</i>
-                    </button>
-                </form>
-            </div>
-        </main>
+			<button class="btn waves-effect waves-light blue darken-3 col s6 " type="submit" name="action">Buscar
+                <i class="material-icons right">search</i>
+            </button>
+		</form>
+	</div>
+	</main>
         <?php require("footer.php");?>
-            <script src="../frameworks/js/materialize.min.js"></script>
-            <script type="text/javascript" src="../JS/header.js"></script>
+                <script type="text/javascript" src = "../JS/Redirect/jquery.redirect.js"></script>
+
+    <script src="../frameworks/js/materialize.min.js"></script>
+    <script type="text/javascript" src="../JS/header.js"></script>
             <script type="text/javascript">
-                $(document).ready(function () {
-                    $(".modal-trigger").leanModal();
-                });
+            	$(document).ready(function(){
+            		$(".modal-trigger").leanModal();
+            	});
             </script>
-    </body>
+</body>
